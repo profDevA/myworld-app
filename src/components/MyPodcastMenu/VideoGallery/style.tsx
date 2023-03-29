fc2a9af947aa55da103360e5760cdf81c918970d
profDevA <toptaldev123@gmail.com>
2023-03-29 10:20:47 +0800

    galery component

--- src/components/MyPodcastMenu/VideoGallery/style.tsx
+++ src/components/MyPodcastMenu/VideoGallery/style.tsx
@@ -3,14 +3,29 @@ import {Dimensions, StyleSheet} from 'react-native';
 const width = Dimensions.get('window').width;
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    paddingBottom: 100,
+  },
   title: {
     fontSize: 23,
     fontFamily: 'Audiowide',
     color: '#fff',
     textAlign: 'center',
     height: 50,
-    backgroundColor: 'green',
     padding: 8,
+  },
+  galeryItem: {
+    height: width * 0.45,
+    width: width * 0.6,
+    marginHorizontal: 15,
+    borderRadius: 30,
+  },
+  image: {
+    height: '100%',
+    flexDirection: 'row',
+  },
+  newBadge: {
+    width: 50,
+    height: 50,
   },
 });
